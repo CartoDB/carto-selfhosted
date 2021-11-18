64e61b3c7ddaf4ac7495b1f7155422167055f01d
Álvaro Manera <amanera@cartodb.com>
2021-11-18 11:40:34 +0100

    check if docker is available

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -6,14 +6,17 @@ command_available() {
   type "$1" >/dev/null 2>&1
 }
 
-if ! command_available wget && command_available yum; then
-  sudo yum install wget
+if ! command_available docker
+then
+    if ! command_available wget && command_available yum; then
+    sudo yum install wget
+    fi
+    wget -qO- https://get.docker.com/ | sh
+    sudo usermod -aG docker $USER
+    newgrp docker
 fi
-wget -qO- https://get.docker.com/ | sh
-sudo usermod -aG docker $USER
-newgrp docker
 
-if ! command -v docker-compose &> /dev/null
+if ! command_available docker-compose
 then
     sudo -E curl -L "https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
