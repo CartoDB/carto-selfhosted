ad20b41749c0f039a94d26b662a80ce7f19cdd48
Manuel López Ruiz <manuellr.git@gmail.com>
2021-12-28 16:58:11 +0100

    Fix install script (#34)

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ DOCKER_MINIMUM_VERSION_MINOR=10
 COMPOSE_MINIMUM_VERSION_MAJOR=2
 COMPOSE_MINIMUM_VERSION_MINOR=1
 
-function check_docker_version () {
+check_docker_version() {
     docker_version_major=$(docker --version | awk  '{ print $3}' | awk -F. '{ print $1 }')
     docker_version_minor=$(docker --version | awk  '{ print $3}' | awk -F. '{ print $2 }')
 
@@ -25,10 +25,12 @@ function check_docker_version () {
     fi
 }
 
-function check_compose_version () {
-    compose_version_major=$(docker-compose --version | awk  '{ print $4}' | awk -F. '{ print $1 }')
-    compose_version_major=${compose_version_major:1} # remove leading v
-    compose_version_minor=$(docker-compose --version | awk  '{ print $4}' | awk -F. '{ print $2 }')
+check_compose_version() {
+    # Docker Compose version v2.1.1
+    # docker-compose version 1.29.2, build unknown
+    compose_version_extracted=$(docker-compose --version | sed 's/^.*version\ //g' | sed 's/[\,\ ].*$//g' | sed 's/v//g')
+    compose_version_major=$(echo "${compose_version_extracted}" | awk -F. '{ print $1 }')
+    compose_version_minor=$(echo "${compose_version_extracted}" | awk -F. '{ print $2 }')
 
     if [ $compose_version_major -ge $COMPOSE_MINIMUM_VERSION_MAJOR ]
     then
@@ -47,7 +49,7 @@ function check_compose_version () {
 
 set -e
 
-if ! command -v docker &> /dev/null
+if ! command -v docker > /dev/null 2>&1
 then
     echo "docker is not installed, you can use the ./scripts/install_docker.sh helper"
     exit 1
@@ -55,7 +57,7 @@ fi
 
 check_docker_version
 
-if ! command -v docker-compose &> /dev/null
+if ! command -v docker-compose > /dev/null 2>&1
 then
     echo "docker-compose is not installed, you can use the ./scripts/install_docker-compose.sh helper"
     exit 1
@@ -80,7 +82,7 @@ if [ ! -f key.json ]; then
     exit 1
 fi
 
-version=`cat VERSION`
+version=$(cat VERSION)
 
 cat customer.env > .env
 echo "" >> .env
